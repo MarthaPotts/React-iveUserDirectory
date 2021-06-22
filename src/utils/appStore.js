4f88f82c986d6ef58ed09fc6583c99656d5a1db4
Martha Potts <marthajpotts7@gmail.com>
2021-06-21 23:52:17 -0400

    mock up html pre-component breakdown due to run error needing resolved

---
+++ src/utils/appStore.js
@@ -0,0 +1,8 @@
+import React, { createContext, useReducer } from 'react'; 
+
+const ToDoContext = createContext({}); 
+const {Provider} = ToDoContext; 
+
+const reducer = (todos, action) => {
+    switch
+}
